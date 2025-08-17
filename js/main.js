42aa22ca3b8cff07799a0433542310ceb20cd271
Hamza HAMZA <hamzaka2008@gmail.com>
2025-08-17 13:27:52 +0300

    add interactive

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
 let categories = document.getElementById("portfolio-category");
 let children = Array.from(categories.children);
-let pics = document.getElementById("pics");
-let picsList = Array.from(pics.children);
+let picsList = Array.from(document.querySelectorAll('.pic'));
 
 
 
 children.forEach(function(ele) {
-    ele.addEventListener("focus", function() {
-        if (ele.classList[1] != 'active') {
-            let active = document.querySelector(".active")
-            active.classList.remove("active");
-            ele.classList.add("active");
-            ele.style.transition = "0.3s";
-            picsList.forEach(function(pic) {
-                let data = pic.children[0].getAttribute("data-work");
-                if (active.textContent == "All") {
-                    pic.style.display = "block";
-                } else if (active.textContent == "Design") {
-                    if (data != "Design") {
-                        pic.style.display = "none"
-                        pic.style.transition = "0.3s"
-                    }
-                }; 
-            })
-        } else {
-            null;
+    ele.addEventListener("click", function() {
+        children.forEach(child => child.classList.remove('active'))
+        ele.classList.add('active')
+        if (ele.classList.contains('active')) {
+            picsList.forEach(pic => pic.style.display = 'none')
+            if(ele.textContent == 'All') {
+                picsList.forEach(pic => pic.style.display = 'block')
+            } else if (ele.textContent == 'Design') {
+                picsList.forEach(pic => {
+                    pic.getAttribute('data-work') == 'Design' ? pic.style.display = 'block' : null;
+                })
+            } else if (ele.textContent == 'Photo') {
+                picsList.forEach(pic => {
+                    pic.getAttribute('data-work') == 'Photo' ? pic.style.display = 'block' : null;
+                })
+            } else if (ele.textContent == 'Code') {
+                picsList.forEach(pic => {
+                    pic.getAttribute('data-work') == 'Code' ? pic.style.display = 'block' : null;
+                })
+            } else if (ele.textContent == 'App') {
+                picsList.forEach(pic => {
+                    pic.getAttribute('data-work') == 'Application' ? pic.style.display = 'block' : null;
+                })
+            }
         }
-    })
+    });
 })
